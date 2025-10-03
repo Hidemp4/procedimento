391702ea75b516fc1f6cb8d3dc613ea40b0b8bed
Hideki <srcai02585@gmail.com>
2025-10-03 18:04:50 -0300

    Atualizao backup

---
+++ app/api/colaboradores/route.ts
@@ -0,0 +1,5 @@
+
+export async function GET(req: Request){
+    console.log("Executado");
+    return req
+}
--- app/colaboradores/data-table.tsx
+++ app/colaboradores/data-table.tsx
@@ -18,36 +18,42 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-
 import { Input } from "@/components/ui/input"
-
 import * as React from "react"
 import { Button } from "@/components/ui/button"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
 }
 
+type DrawerType = "filter" | "insert" | null;
+
 export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  // Controle de estados
+  // 1. Controle para ordenação
   const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  // 2. Controle para filtros
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
   );
 
+  // 3. Controle para ativação dos Drawers (Filtros, Inserir colaborador e +)
+  const [activeDrawer, setActiveDrawer] = React.useState<DrawerType>();
+
+  // Lógica para fechar o Drawer
+  const handleDrawerClose = (isOpen: boolean) => {
+    if (!isOpen) {
+      setActiveDrawer(null);
+    }
+  }
+
+  // Config da tabela
   const table = useReactTable({
     data,
     columns,
@@ -59,7 +65,7 @@ export function DataTable<TData, TValue>({
     state: {
       sorting,
       columnFilters,
-    },
+    }
   })
 
   return (
@@ -67,30 +73,31 @@ export function DataTable<TData, TValue>({
       <Table>
         <TableHeader>
           <div className="p-4">
-            <Dialog>
-              <div className="flex gap-5 items-center">
-                <DialogTrigger asChild>
-                  <Button variant="outline">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                    </svg>
-                    <span>Filtrar</span>
-                  </Button>
-                </DialogTrigger>
+            {/* Botões para ativação dos Drawers no Header da tabela */}
+            <div className="flex gap-5 items-center">
 
-                <div className="h-[30px] w-px border-r border-control"></div>
+              {/* Botão para Filtros */}
+              <Button variant="outline" id="filterColaborador" onClick={() => setActiveDrawer("filter")}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
+                </svg>
+                <span>Filtrar</span>
+              </Button>
 
-                <DialogTrigger>
-                  <Button variant="outline">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    <span>Inserir</span>
-                  </Button>
-                </DialogTrigger>
-              </div>
+              {/* Separador de botões */}
+              <div className="h-[30px] w-px border-r border-control"></div>
 
+              {/* Botão para insert de colaboradores */}
+              <Button variant="outline" onClick={() => setActiveDrawer("insert")}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+                <span>Inserir</span>
+              </Button>
+            </div>
 
+            {/* Drawer de Filtro */}
+            <Dialog open={activeDrawer === "filter"} onOpenChange={handleDrawerClose}>
               <DialogContent className="sm:max-w-md">
                 <DialogHeader>
                   <DialogTitle>Filtrar por:</DialogTitle>
@@ -106,7 +113,7 @@ export function DataTable<TData, TValue>({
                       onChange={(event) =>
                         table.getColumn("nome")?.setFilterValue(event.target.value)
                       }
-                      className="max-w-sm"
+                    className="max-w-sm"
                     />
                   </div>
                 </div>
@@ -153,28 +160,51 @@ export function DataTable<TData, TValue>({
               </DialogContent>
             </Dialog>
 
+            {/* Drawer de Insert */}
+            <Dialog open={activeDrawer === "insert"} onOpenChange={handleDrawerClose}>
+              <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                  <DialogTitle>Inserir novo Colaborador</DialogTitle>
+                </DialogHeader>
+                <div className="flex items-center gap-2">
+                  <DialogDescription>
+                    Nome:
+                  </DialogDescription>
+                  <div className="grid flex-1 gap-2">
+                    <Input />
+                  </div>
+                </div>
 
+                <div className="flex items-center gap-2">
+                  <DialogDescription>
+                    Mátricula:
+                  </DialogDescription>
+                  <div className="grid flex-1 gap-2">
+                    <Input />
+                  </div>
+                </div>
 
+                <div className="flex items-center gap-2">
+                  <DialogDescription>
+                    Cargo:
+                  </DialogDescription>
+                  <div className="grid flex-1 gap-2">
+                    <Input />
+                  </div>
+                </div>
 
+                <DialogFooter className="sm:justify-start">
+                  <DialogClose asChild>
+                    <Button type="button" variant="secondary">
+                      Fechar
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </div>
 
-
-
-          {/*
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                </svg>
-          
-          <Input
-              placeholder="Filtrar Nomes..."
-              value={(table.getColumn("nome")?.getFilterValue() as string) ?? ""}
-              onChange={(event) =>
-                table.getColumn("nome")?.setFilterValue(event.target.value)
-              }
-              className="max-w-sm"
-            /> */}
-
-
+          {/* Colunas da tabela */}
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
@@ -208,7 +238,7 @@ export function DataTable<TData, TValue>({
             ))) : (
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
+                Sem resultados.
               </TableCell>
             </TableRow>
           )}
