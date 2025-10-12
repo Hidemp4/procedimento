ab40564a5ed4f5692055b41c485656deaa253d0f
Hideki <srcai02585@gmail.com>
2025-10-11 21:53:08 -0300

    Atualização Feature Filtros

--- app/colaboradores/data-table.tsx
+++ app/colaboradores/data-table.tsx
@@ -1,5 +1,12 @@
 "use client"
-
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -20,15 +27,13 @@ import {
 } from "@/components/ui/table"
 import { Input } from "@/components/ui/input"
 import * as React from "react"
+import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
 }
-
-type DrawerType = "filter" | "insert" | null;
 
 export function DataTable<TData, TValue>({
   columns,
@@ -42,16 +47,6 @@ export function DataTable<TData, TValue>({
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
   );
-
-  // 3. Controle para ativação dos Drawers (Filtros, Inserir colaborador e +)
-  const [activeDrawer, setActiveDrawer] = React.useState<DrawerType>();
-
-  // Lógica para fechar o Drawer
-  const handleDrawerClose = (isOpen: boolean) => {
-    if (!isOpen) {
-      setActiveDrawer(null);
-    }
-  }
 
   // Config da tabela
   const table = useReactTable({
@@ -68,6 +63,20 @@ export function DataTable<TData, TValue>({
     }
   })
 
+  const [countFilters, setCountFilters] = useState(0);
+
+  // Pega os valores dos inputs (ou filtros)
+  const nome = (table.getColumn("nome")?.getFilterValue() as string) ?? "";
+  const matricula = (table.getColumn("matricula")?.getFilterValue() as string) ?? "";
+  const cargo = (table.getColumn("cargo")?.getFilterValue() as string) ?? "";
+
+  useEffect(() => {
+    // Conta quantos filtros estão "ativos"
+    const ativos = [nome, matricula, cargo].filter((v) => v && v.trim() !== "").length;
+    console.log("Valor ativos: ", ativos)
+    setCountFilters(ativos);
+  }, [nome, matricula, cargo]);
+
   return (
     <div className="overflow-hidden rounded-md border">
       <Table>
@@ -77,131 +86,152 @@ export function DataTable<TData, TValue>({
             <div className="flex gap-5 items-center">
 
               {/* Botão para Filtros */}
-              <Button variant="outline" id="filterColaborador" onClick={() => setActiveDrawer("filter")}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                </svg>
-                <span>Filtrar</span>
-              </Button>
+              <Sheet>
+                <SheetTrigger>
+                  {/* Contador de Filtros */}
+                  {countFilters !== 0 ?
+                    (
+                      <div className="relative p-1">
+                        <Button variant="outline">
+                          <span className="text-white bg-red-800 size-4 rounded-full absolute right-0 top-0 flex justify-center items-center">{countFilters}</span>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
+                          </svg>
+                          <span>Filtrar</span>
+                        </Button>
+                      </div>
+                    ) : (
+                      <div className="relative p-1">
+                        <Button variant="outline">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
+                          </svg>
+                          <span>Filtrar</span>
+                        </Button>
+                      </div>
+                    )}
+                </SheetTrigger>
+                <SheetContent>
+                  <SheetHeader>
+                    <SheetTitle>Filtrar por:</SheetTitle>
+                    <SheetDescription className="space-y-5">
+                      <div className="flex flex-col justify-start gap-2">
+                        <label htmlFor="nome-colaborador">
+                          Nome:
+                        </label>
+                        <div className="grid flex-1 gap-2">
+                          <Input
+                            id="nome-colaborador"
+                            placeholder="Nome do colaborador"
+                            value={nome}
+                            onChange={(e) => table.getColumn("nome")?.setFilterValue(e.target.value)}
+                            className="max-w-sm"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex flex-col justify-start gap-2">
+                        <label htmlFor="matricula-colaborador">
+                          Matrícula:
+                        </label>
+                        <div className="grid flex-1 gap-2">
+                          <Input
+                            id="matricula-colaborador"
+                            placeholder="Ex: 21474"
+                            value={matricula}
+                            onChange={(e) => table.getColumn("matricula")?.setFilterValue(e.target.value)}
+                            className="max-w-sm"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col justify-start gap-2">
+                        <label htmlFor="cargo-colaborador">
+                          Cargo:
+                        </label>
+                        <div className="grid flex-1 gap-2">
+                          <Input
+                            placeholder="Ex: Auxiliar de Operações"
+                            value={cargo}
+                            onChange={(e) => table.getColumn("cargo")?.setFilterValue(e.target.value)}
+                            className="max-w-sm"
+                          />
+                        </div>
+                      </div>
+                    </SheetDescription>
+                  </SheetHeader>
+                </SheetContent>
+              </Sheet>
 
               {/* Separador de botões */}
               <div className="h-[30px] w-px border-r border-control"></div>
 
-              {/* Botão para insert de colaboradores */}
-              <Button variant="outline" onClick={() => setActiveDrawer("insert")}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-                <span>Inserir</span>
-              </Button>
+              {/* Botão para Inserir */}
+              <Sheet>
+                <SheetTrigger>
+                  <Button variant="outline">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Inserir</span>
+                  </Button>
+                </SheetTrigger>
+                <SheetContent>
+                  <SheetHeader>
+                    <SheetTitle>Filtrar por:</SheetTitle>
+                    <SheetDescription className="space-y-5">
+                      <div className="flex flex-col justify-start gap-2">
+                        <label htmlFor="nome-colaborador">
+                          Nome:
+                        </label>
+                        <div className="grid flex-1 gap-2">
+                          <Input
+                            id="nome-colaborador"
+                            placeholder="Nome do colaborador"
+                            value={(table.getColumn("nome")?.getFilterValue() as string) ?? ""}
+                            onChange={(event) =>
+                              table.getColumn("nome")?.setFilterValue(event.target.value)
+                            }
+                            className="max-w-sm"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex flex-col justify-start gap-2">
+                        <label htmlFor="matricula-colaborador">
+                          Matrícula:
+                        </label>
+                        <div className="grid flex-1 gap-2">
+                          <Input
+                            id="matricula-colaborador"
+                            placeholder="Ex: 21474"
+                            value={(table.getColumn("matricula")?.getFilterValue() as string) ?? ""}
+                            onChange={(event) =>
+                              table.getColumn("matricula")?.setFilterValue(event.target.value)
+                            }
+                            className="max-w-sm"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col justify-start gap-2">
+                        <label htmlFor="cargo-colaborador">
+                          Cargo:
+                        </label>
+                        <div className="grid flex-1 gap-2">
+                          <Input
+                            placeholder="Ex: Auxiliar de Operações"
+                            value={(table.getColumn("cargo")?.getFilterValue() as string) ?? ""}
+                            onChange={(event) =>
+                              table.getColumn("cargo")?.setFilterValue(event.target.value)
+                            }
+                            className="max-w-sm"
+                          />
+                        </div>
+                      </div>
+                    </SheetDescription>
+                  </SheetHeader>
+                </SheetContent>
+              </Sheet>
             </div>
-
-            {/* Drawer de Filtro */}
-            <Dialog open={activeDrawer === "filter"} onOpenChange={handleDrawerClose}>
-              <DialogContent className="sm:max-w-md">
-                <DialogHeader>
-                  <DialogTitle>Filtrar por:</DialogTitle>
-                </DialogHeader>
-                <div className="flex items-center gap-2">
-                  <DialogDescription>
-                    Nome:
-                  </DialogDescription>
-                  <div className="grid flex-1 gap-2">
-                    <Input
-                      placeholder="Nome do colaborador"
-                      value={(table.getColumn("nome")?.getFilterValue() as string) ?? ""}
-                      onChange={(event) =>
-                        table.getColumn("nome")?.setFilterValue(event.target.value)
-                      }
-                    className="max-w-sm"
-                    />
-                  </div>
-                </div>
-
-                <div className="flex items-center gap-2">
-                  <DialogDescription>
-                    Mátricula:
-                  </DialogDescription>
-                  <div className="grid flex-1 gap-2">
-                    <Input
-                      placeholder="Ex: 21474"
-                      value={(table.getColumn("matricula")?.getFilterValue() as string) ?? ""}
-                      onChange={(event) =>
-                        table.getColumn("matricula")?.setFilterValue(event.target.value)
-                      }
-                      className="max-w-sm"
-                    />
-                  </div>
-                </div>
-
-                <div className="flex items-center gap-2">
-                  <DialogDescription>
-                    Cargo:
-                  </DialogDescription>
-                  <div className="grid flex-1 gap-2">
-                    <Input
-                      placeholder="Ex: Auxiliar de Operações"
-                      value={(table.getColumn("cargo")?.getFilterValue() as string) ?? ""}
-                      onChange={(event) =>
-                        table.getColumn("cargo")?.setFilterValue(event.target.value)
-                      }
-                      className="max-w-sm"
-                    />
-                  </div>
-                </div>
-
-                <DialogFooter className="sm:justify-start">
-                  <DialogClose asChild>
-                    <Button type="button" variant="secondary">
-                      Fechar
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-
-            {/* Drawer de Insert */}
-            <Dialog open={activeDrawer === "insert"} onOpenChange={handleDrawerClose}>
-              <DialogContent className="sm:max-w-md">
-                <DialogHeader>
-                  <DialogTitle>Inserir novo Colaborador</DialogTitle>
-                </DialogHeader>
-                <div className="flex items-center gap-2">
-                  <DialogDescription>
-                    Nome:
-                  </DialogDescription>
-                  <div className="grid flex-1 gap-2">
-                    <Input />
-                  </div>
-                </div>
-
-                <div className="flex items-center gap-2">
-                  <DialogDescription>
-                    Mátricula:
-                  </DialogDescription>
-                  <div className="grid flex-1 gap-2">
-                    <Input />
-                  </div>
-                </div>
-
-                <div className="flex items-center gap-2">
-                  <DialogDescription>
-                    Cargo:
-                  </DialogDescription>
-                  <div className="grid flex-1 gap-2">
-                    <Input />
-                  </div>
-                </div>
-
-                <DialogFooter className="sm:justify-start">
-                  <DialogClose asChild>
-                    <Button type="button" variant="secondary">
-                      Fechar
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
           </div>
 
           {/* Colunas da tabela */}
@@ -245,6 +275,6 @@ export function DataTable<TData, TValue>({
         </TableBody>
 
       </Table>
-    </div>
+    </div >
   )
 }
